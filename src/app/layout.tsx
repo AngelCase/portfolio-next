7480e93a60a6513a3452a03312a8ec6fa89dbe40
AngelCase <inoino884@gmail.com>
2024-05-11 16:17:39 +0900

    add navigation bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../styles/reset.css";
 import "../styles/globals.scss";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +18,30 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header>
+          <nav className="nav">
+            <ul className="list">
+              <li className="list__item">
+                <Link href="/">TOP</Link>
+              </li>
+              <div className="separator"></div>
+              <li className="list__item">
+                <Link href="/skills">SKILLS</Link>
+              </li>
+              <div className="separator"></div>
+              <li className="list__item">
+                <Link href="/works">WORKS</Link>
+              </li>
+              <div className="separator"></div>
+              <li className="list__item">
+                <Link href="/contact">CONTACT</Link>
+              </li>
+            </ul>
+          </nav>
+          {children}
+        </header>
+      </body>
     </html>
   );
 }
