7cca544d3c0de1d80f02d3b5a9d93ad05cb0691e
AngelCase <inoino884@gmail.com>
2024-05-12 15:02:13 +0900

    add PageHeading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,5 @@
-"use client";
-
-import { useTypeScramble } from "@/hooks/useTypeScramble";
-import { useEffect } from "react";
+import PageHeading from "@/components/PageHeading";
 
 export default function Home() {
-  const { scramble, current } = useTypeScramble("WELCOME TO MY PORTFOLIO");
-
-  // NOTE: 何度も実行されないようuseEffectに空の依存配列を渡している
-  useEffect(() => {
-    scramble();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  return <h1>{current}</h1>;
+  return <PageHeading text="WELCOME TO MY PORTFOLIO"></PageHeading>;
 }
---
+++ src/components/PageHeading.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { useTypeScramble } from "@/hooks/useTypeScramble";
+import { useEffect } from "react";
+
+export default function PageHeading({ text }: { text: string }) {
+  const { scramble, current } = useTypeScramble(text);
+
+  // NOTE: 何度も実行されないようuseEffectに空の依存配列を渡している
+  useEffect(() => {
+    scramble();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return <h1 className="text">{current}</h1>;
+}
